2e344a75494b36e6faceddcb5e3845a678dfc0a8
kertase <kertase@gmail.com>
2016-08-26 09:16:43 +0000

    new file:   correctgps 	new file:   gpx.fmt 	modified:   mapillary_scripts/tiftomapil.sh 	modified:   mapillary_scripts/upload.sh

--- mapillary_scripts/tiftomapil.sh
+++ mapillary_scripts/tiftomapil.sh
@@ -1,5 +1,5 @@
-for i in `ls /home/lukas/alsenet/panos/original/panos4`
+for i in `ls result_146236708{0..2}_*.tif result_146236707{6..9}_*.tif`
 do
-n=$(basename "$i" -0-25-1.jpeg)
-convert -level 0%,25%,1 /mnt/usb/rawdata/00-0e-64-08-1e-39/master/1460401040/segment/1462366612/panos/"$n".tif -define jpeg:extent=20000kb ./"$i"
+
+convert -level 0%,25%,1 /mnt/usb/rawdata/00-0e-64-08-1e-39/master/1460401040/segment/1462366612/panos/"$i" -define jpeg:extent=20000kb /home/lukas/alsenet/panos/panos5/`basename $i .tif`.jpg
 done
