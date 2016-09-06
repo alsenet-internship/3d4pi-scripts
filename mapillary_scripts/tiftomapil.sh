d803a40744e666c8852f801bb4fb297fd8c15117
kertase <kertase@gmail.com>
2016-09-06 09:58:45 +0000

    update of scripts with little edits

--- mapillary_scripts/tiftomapil.sh
+++ mapillary_scripts/tiftomapil.sh
@@ -1,5 +1,6 @@
-for i in `ls result_146236708{0..2}_*.tif result_146236707{6..9}_*.tif`
+cd /mnt/usb/rawdata/00-0e-64-08-1e-39/master/1460401040/segment/1462366612/panos/
+for i in `ls result_1462368{169..211}_*.tif`
 do
 
-convert -level 0%,25%,1 /mnt/usb/rawdata/00-0e-64-08-1e-39/master/1460401040/segment/1462366612/panos/"$i" -define jpeg:extent=20000kb /home/lukas/alsenet/panos/panos5/`basename $i .tif`.jpg
+convert -level 0%,25%,1 /mnt/usb/rawdata/00-0e-64-08-1e-39/master/1460401040/segment/1462366612/panos/"$i" -define jpeg:extent=20000kb /home/lukas/alsenet/panos/panos6/`basename $i .tif`.jpg
 done
