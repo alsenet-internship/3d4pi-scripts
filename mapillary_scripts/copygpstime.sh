f40da3d91a8af4837cbb7ab9fda1a47e18f5fabe
kertase <kertase@gmail.com>
2016-09-06 13:05:37 +0000

    update even more

--- mapillary_scripts/copygpstime.sh
+++ mapillary_scripts/copygpstime.sh
@@ -3,6 +3,7 @@ for i in $@
 do
 	gdt=$(exiftool -s3 -GPSDateTime "$i")
 	ssec=$(echo $gdt | sed 's/.*\(.\).$/\1/')
-
-	exiftool -overwrite_original -DateTimeOriginal="$gdt" -SubSecTimeOriginal="$ssec" "$i"
+if [ -n "$gdt"  ]; then
+ exiftool -overwrite_original -DateTimeOriginal="$gdt" -SubSecTimeOriginal="$ssec" "$i"
+fi
 done
--- mapillary_scripts/upload.sh
+++ mapillary_scripts/upload.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 sd=$(dirname $0)
-if [ "$#" -ne 4 ] || [ "$#" -ne 5 ] ; then
+if [ "$#" -ne 4 ]  ; then
     echo "syntax: upload.sh FOLDER EMAIL USERNAME PASSWORD [sequence_id]"
 else
 rename .jpeg .jpg $1/*
 $sd/copygpstime.sh $1/*.jpg #needed if time was not copied from original
-$sd/gpscorrect $1
+$sd/correctgps $1
 python2 $sd/interpolate_direction.py $1 180
 $sd/AddSphereXMP.sh $1/*.jpg
  MAPILLARY_EMAIL=$2 MAPILLARY_USERNAME=$3 MAPILLARY_PASSWORD=$4 python2 $sd/add_mapillary_tag_from_exif.py $1  $5
 echo "Done Tagging, starting compression"
-size=$(wc -c <"$file")
 for i in `ls $1`; do
-if [ $i -ge 20000000 ]; then
+size=$(wc -c <"$1/$i")
+if [ $size -ge 20000000 ]; then
 mogrify -define jpeg:extent=20000kb $1/$i
 fi
+done
 echo "Done compressing press y to confirm upload"
 read -n 1 -p "Input Selection:" mainmenuinput
   if [ "$mainmenuinput" = "y" ]; then
+echo "no upload"
 python2 $sd/upload.py $1
 fi
 fi
