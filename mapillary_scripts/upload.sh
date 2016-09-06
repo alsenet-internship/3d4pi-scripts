a1764be6eeb0ba75a41115d73832f38a987a439f
kertase <kertase@gmail.com>
2016-09-06 12:11:27 +0000

    more update

--- mapillary_scripts/upload.sh
+++ mapillary_scripts/upload.sh
@@ -5,14 +5,15 @@ if [ "$#" -ne 4 ] || [ "$#" -ne 5 ] ; then
 else
 rename .jpeg .jpg $1/*
 $sd/copygpstime.sh $1/*.jpg #needed if time was not copied from original
-#$sd/gpscorrect $1
+$sd/gpscorrect $1
 python2 $sd/interpolate_direction.py $1 180
 $sd/AddSphereXMP.sh $1/*.jpg
  MAPILLARY_EMAIL=$2 MAPILLARY_USERNAME=$3 MAPILLARY_PASSWORD=$4 python2 $sd/add_mapillary_tag_from_exif.py $1  $5
-#echo "Done Tagging, starting compression"
+echo "Done Tagging, starting compression"
 size=$(wc -c <"$file")
-if [ $size -ge 20000000 ]; then
-mogrify -define jpeg:extent=20000kb $1/*.jpg
+for i in `ls $1`; do
+if [ $i -ge 20000000 ]; then
+mogrify -define jpeg:extent=20000kb $1/$i
 fi
 echo "Done compressing press y to confirm upload"
 read -n 1 -p "Input Selection:" mainmenuinput
